1694a5a30f017c054d0378922dae6e4135786a64
SashaGinSan <SashaGinSan@gmail.com>
2015-07-11 12:12:47 +0300

    Alexander add Person

--- summer_practice/console/src/main/java/ru/relex/summer_practice/console/Main.java
+++ summer_practice/console/src/main/java/ru/relex/summer_practice/console/Main.java
@@ -3,15 +3,16 @@ package ru.relex.summer_practice.console;
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
 
-import ru.relex.summer_practice.db.test;
+import ru.relex.summer_practice.db.Person;
+
 
 public class Main {
 	public static void main( String[] args )
     {
 		EntityManager em = Persistence.createEntityManagerFactory("COLIBRI").createEntityManager();
 		em.getTransaction().begin();
-        test t = new test();
-        t.setName("проверка");
+        Person t = new Person();
+        t.setFullname("name");
         em.merge(t);
         em.getTransaction().commit();
         System.out.println("проверочка");
