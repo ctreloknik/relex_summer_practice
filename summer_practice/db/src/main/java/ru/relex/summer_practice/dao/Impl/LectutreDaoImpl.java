f85d777ea83c64a77f021bdbf8198b3702659e1c
EugeneGorbachev <aurantiaco@yandex.ru>
2015-07-12 22:24:53 +0300

    Eugene: fixed: LecturePersonDaoImpl.java; realized LectureDao.java & LectureRoomDaoImpl.java, LectureDao.java & LectutreDaoImpl.java, CourseDao.java & CourseDaoImpl.java, ConferenceDao.java & ConferenceDaoImpl.java.

--- summer_practice/db/src/main/java/ru/relex/summer_practice/dao/Impl/LectutreDaoImpl.java
+++ summer_practice/db/src/main/java/ru/relex/summer_practice/dao/Impl/LectutreDaoImpl.java
@@ -2,9 +2,82 @@ package ru.relex.summer_practice.dao.Impl;
 
 import ru.relex.summer_practice.dao.LectureDao;
 import ru.relex.summer_practice.db.Lecture;
+import ru.relex.summer_practice.db.Ticket;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.Collection;
+import java.util.Date;
 
 /**
- * Created by Nikita on 12.07.2015.
+ * Created by Eugene on 12.07.2015.
  */
 public class LectutreDaoImpl extends GenericCrudDaoImpl<Lecture, Long> implements LectureDao{
+    @Override
+    public Lecture Create(Lecture lecture) {
+        return super.Create(lecture);
+    }
+
+    @Override
+    public Lecture Read(Long id) {
+        return super.Read(id);
+    }
+
+    @Override
+    public Collection<Lecture> ReadAll() {
+        return super.ReadAll();
+    }
+
+    @Override
+    public Lecture Update(Lecture lecture) {
+        return super.Update(lecture);
+    }
+
+    @Override
+    public void Delete(Long id) {
+        super.Delete(id);
+    }
+
+    public String getTopic(Lecture lecture) {
+        EntityManagerFactory emf = null;
+        EntityManager em = null;
+        try {
+            emf = Persistence.createEntityManagerFactory("PERSISTENCE");
+            em = emf.createEntityManager();
+            Long lecture_id = lecture.getId();
+            return em.find(Lecture.class, lecture_id).getTopic();
+        } finally {
+            if (em != null ) em.close();
+            if (emf != null) emf.close();
+        }
+    }
+
+    public Date getDatetime(Lecture lecture) {
+        EntityManagerFactory emf = null;
+        EntityManager em = null;
+        try {
+            emf = Persistence.createEntityManagerFactory("PERSISTENCE");
+            em = emf.createEntityManager();
+            Long lecture_id = lecture.getId();
+            return em.find(Lecture.class, lecture_id).getDatetime();
+        } finally {
+            if (em != null ) em.close();
+            if (emf != null) emf.close();
+        }
+    }
+
+    public String getDescription(Lecture lecture) {
+        EntityManagerFactory emf = null;
+        EntityManager em = null;
+        try {
+            emf = Persistence.createEntityManagerFactory("PERSISTENCE");
+            em = emf.createEntityManager();
+            Long lecture_id = lecture.getId();
+            return em.find(Lecture.class, lecture_id).getDescription();
+        } finally {
+            if (em != null ) em.close();
+            if (emf != null) emf.close();
+        }
+    }
 }
--- summer_practice/db/src/main/java/ru/relex/summer_practice/dao/LectureRoomDao.java
+++ summer_practice/db/src/main/java/ru/relex/summer_practice/dao/LectureRoomDao.java
@@ -6,4 +6,6 @@ import ru.relex.summer_practice.db.LectureRoom;
  * Created by Nikita on 12.07.2015.
  */
 public interface LectureRoomDao extends GenericCrudDao<LectureRoom, Long> {
+    public String getNumber(LectureRoom lectureRoom);
+    public int getSeating(LectureRoom lectureRoom);
 }
