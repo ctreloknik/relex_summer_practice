2d9d09f270470f092b7fee896dcd8b90273ead9d
Valaar <nikita.chebotaryov@gmail.com>
2015-07-13 02:19:13 +0300

    NIK. Added testing class with two methods

--- summer_practice/console/src/main/java/ru/relex/summer_practice/console/Main.java
+++ summer_practice/console/src/main/java/ru/relex/summer_practice/console/Main.java
@@ -1,13 +1,9 @@
 package ru.relex.summer_practice.console;
 
-
-import ru.relex.summer_practice.dao.Impl.PersonDaoImpl;
-import ru.relex.summer_practice.db.Person;
-
 public class Main {
 	public static void main( String[] args )
     {
-		PersonDaoImpl personDao = new PersonDaoImpl();
-        System.out.print(personDao.ReadAll());
+        DBModuleTest.personTest();
+        DBModuleTest.RolesTest();
     }
 }
--- summer_practice/db/src/main/java/ru/relex/summer_practice/db/Roles.java
+++ summer_practice/db/src/main/java/ru/relex/summer_practice/db/Roles.java
@@ -45,4 +45,9 @@ public class Roles {
     public Set<PersonLectureRole> getPersonLectureRole() {
         return personLectureRole;
     }
+
+    @Override
+    public String toString(){
+        return this.name;
+    }
 }
