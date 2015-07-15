115c0139a3a1204133e5ab4acb878597e2d4c582
Nikita Chebotaryov <nikita.chebotaryov@gmail.com>
2015-07-15 21:32:25 +0400

    NIK. Some fixes in FoundersDaoImpl.java

--- summer_practice/db/src/main/java/ru/relex/summer_practice/dao/Impl/FoundersDaoImpl.java
+++ summer_practice/db/src/main/java/ru/relex/summer_practice/dao/Impl/FoundersDaoImpl.java
@@ -5,8 +5,8 @@ import ru.relex.summer_practice.db.Conference;
 import ru.relex.summer_practice.db.Founders;
 import ru.relex.summer_practice.db.Person;
 
-import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by Nikita on 12.07.2015.
@@ -16,17 +16,17 @@ public class FoundersDaoImpl extends GenericCrudDaoImpl<Founders, Long> implemen
         super(Founders.class);
     }
 
-    public Collection getConferensesByPerson(Person person) {
+    public List<Founders> getConferensesByPerson(Person person) {
         String jpql = "select f from FOUNDERS f where f.person = :person";
-        HashMap<String, Object> parametres = new HashMap<>();
-        parametres.put("person",person);
-        return this.EexecuteQuery(jpql,parametres);
+        HashMap<String, Object> parameters = new HashMap<>();
+        parameters.put("person",person);
+        return this.EexecuteQuery(jpql, parameters);
     }
 
-    public Collection getPersonsByConference(Conference conference) {
+    public List<Founders> getPersonsByConference(Conference conference) {
         String jpql = "select f from FOUNDERS f where f.conference = :conference";
-        HashMap<String, Object> parametres = new HashMap<>();
-        parametres.put("conference",conference);
-        return this.EexecuteQuery(jpql,parametres);
+        HashMap<String, Object> parameters = new HashMap<>();
+        parameters.put("conference",conference);
+        return this.EexecuteQuery(jpql, parameters);
     }
 }
