b6e959af396af3ced2153376516285a72439ca4d
Valaar <nikita.chebotaryov@gmail.com>
2015-07-20 20:32:40 +0300

    NIK. Update hibernate classes (*DAO and *DAOImpl + *Service): Founders, LecturePerson, Person, PersonLectureRole.

--- summer_practice/db/src/main/java/ru/relex/summer_practice/dao/FoundersDao.java
+++ summer_practice/db/src/main/java/ru/relex/summer_practice/dao/FoundersDao.java
@@ -4,12 +4,12 @@ import ru.relex.summer_practice.db.Conference;
 import ru.relex.summer_practice.db.Founders;
 import ru.relex.summer_practice.db.Person;
 
-import java.util.Collection;
+import java.util.List;
 
 /**
  * Created by Sasha on 12.07.2015.
  */
 public interface FoundersDao extends GenericCrudDao<Founders, Long> {
-    public Collection getConferensesByPerson(Person person);
-    public Collection getPersonsByConference(Conference conference);
+    public List<Founders> getConferensesByPerson(Person person);
+    public List<Founders> getPersonsByConference(Conference conference);
 }
